c019ed1504d992a9c378ef5818db70efdc5451e7
planeslorena <planeslorena@gmail.com>
2023-11-03 16:15:00 -0300

    creacion componente contacto

---
+++ src/app/componentes/contacto/Contacto.tsx
@@ -0,0 +1,15 @@
+import './Contacto.css'
+
+export const Contacto = () => {
+  
+  return (
+    <div className='d-flex row'>
+        <div className='d-flex col redes'>
+            <a href="https://www.facebook.com" target="_blank">
+                <i className="bi bi-facebook"></i> 
+            </a>
+            <a href=""><i className="bi bi-instagram "></i> </a>
+        </div>
+    </div>
+  );
+}
