ba0499be3efa22eeeac0e558337de6ee174617b3
Juan Andreu <juandreu2002@gmail.com>
2023-11-03 18:39:14 -0300

    Se agrego componente serviciosHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
+import '/page.css'
 import { Logo } from "./componentes/logo/logo"
 import { Nav } from "./componentes/nav/nav"
 import { Footer } from "./componentes/footer/footer"
+import ServiciosComponent from "./componentes/serviciosHome/servicios";
 
 export default function Home() {
   return (
-   <>
+   <div className="containerBody">
    <header>
     <Logo></Logo>
     <Nav></Nav>
 
    </header>
    <main>
-
+   <ServiciosComponent/>
    </main>
    <Footer></Footer>
 
-   </>
+   </div>
   )
 }
 
