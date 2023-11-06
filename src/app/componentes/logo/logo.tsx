7f305088e598c9dafe52761b8b116d8c7c528a1d
Luna <lunacristofanelli@gmail.com>
2023-11-06 16:12:03 -0300

    Sume galeria a develop

--- src/app/componentes/logo/logo.tsx
+++ src/app/componentes/logo/logo.tsx
@@ -2,23 +2,6 @@ import './logo.css'
 
 export const Logo = () => {
 
-<<<<<<< HEAD
-    return (
-        
-            <nav className="fondologo">
-                <div className="container logo">
-
-                    <a className="navbar-brand" href="#">
-                        <img src="/imagenes/logomunicipio.jpeg" alt="logo de sierras bayas" width="450px" height="170px"/>
-                    </a>
-                </div>
-
-
-            </nav>
-
-        
-
-=======
     return ( 
         <nav className="fondologo">
             <div className="container logo">
@@ -27,6 +10,5 @@ export const Logo = () => {
                 </a>
             </div>
         </nav>
->>>>>>> develop
     );
 }   
