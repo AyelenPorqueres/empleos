e8cddcb07930d3858b65eeed749e9dd1e8a357ad
Luna <lunacristofanelli@gmail.com>
2023-11-07 20:32:58 -0300

    Terminada galeria de imágenes

--- src/app/componentes/carrousel/carrousel.tsx
+++ src/app/componentes/carrousel/carrousel.tsx
@@ -9,18 +9,18 @@ export function Carrousel() {
   return (
     <Carousel className="containerImagenes container-fluid">
       <Carousel.Item>
-        <img className="img-carrousel" src="Imagenes/ImagenInicio1.jpeg" alt="" />
+        <img className="img-carrousel" src="Imagenes/Imagen1.png" alt="" />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="img-carrousel" src="Imagenes/ImagenInicio2.jpeg" alt="" />
+        <img className="img-carrousel" src="Imagenes/Imagen2.png" alt="" />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        <img className="img-carrousel" src="Imagenes/ImagenInicio3.jpeg" alt="" />
+        <img className="img-carrousel" src="Imagenes/Imagen3.png" alt="" />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
