9d89d3fcc7703fdf673279be68742e124e9d650b
AyelenPorqueres <ayelen.porqueres04@gmail.com>
2023-11-07 16:37:57 -0300

    cambios en el inicio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css';
+
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
