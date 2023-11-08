884bcdc62274299eac40938daa39d6095d6a6cb9
Ruben-Torres25 <ruben.010.torres@gmail.com>
2023-11-08 20:21:20 -0300

    Barra buscadora con boton

--- src/app/(pages)/cardempleos/page.tsx
+++ src/app/(pages)/cardempleos/page.tsx
@@ -3,14 +3,14 @@ import { Nav } from "../../componentes/nav/nav"
 import { CardEmpleos } from "../../componentes/cardEmpleos/cardEmpleos"
 import { CardContainer } from "../../componentes/cardContainer/cardContainer"
 import { datosEmpleos } from "../../services/datosEmpleos"
-
+import { FiltroEmpleos } from "@/app/componentes/filtroEmpleos/filtroEmpleos"
 export default function Home() {
   return (
     <>
       <header>
         <Logo></Logo>
         <Nav></Nav>
-
+        <FiltroEmpleos />
       </header>
       <main>
         <CardContainer>
--- src/app/componentes/filtroEmpleos/filtroEmpleos.tsx
+++ src/app/componentes/filtroEmpleos/filtroEmpleos.tsx
@@ -1,24 +1,32 @@
 import './filtroEmpleos.css'
 export const FiltroEmpleos = () => {
     return (
-        <div className="contenedor">
-            <table>
-                <tr>
-                    <label className="">
-                        <select className="fondoBuscadores selector" name="" id="">
-                            <option value="">Carga Horaria</option>
-                            <option value="">Rubro</option>
-                            <option value="">Puesto</option>
-                        </select>
-                    </label>
+        <>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"></link>
+            <div className="contenedor">
+                <table>
+                    <tr>
+                        <label className="">
+                            <select className="fondoBuscadores selector" name="" id="">
+                                <option value="">Carga Horaria</option>
+                                <option value="">Rubro</option>
+                                <option value="">Puesto</option>
+                            </select>
+                        </label>
 
-                    <th>
-                        <form  action="search">
-                            <input id="letraBuscador" className="fondoBuscadores buscador" type="search" placeholder="Search" aria-label="Search" />
-                        </form>
-                    </th>
-                </tr>
-            </table>
-        </div >
+                        <th>
+                            <form action="search">
+                                <div className="buscar">
+                                    <input type="text" placeholder='buscar' required />
+                                    <div className='btn'>
+                                        <i className='fa fa-search'></i>
+                                    </div>
+                                </div>
+                            </form>
+                        </th>
+                    </tr>
+                </table>
+            </div >
+        </>
     )
 }
