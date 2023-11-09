6ea9df3e67189f1dd6021db8c866c9c90f784e57
planeslorena <planeslorena@gmail.com>
2023-11-09 15:42:34 -0300

    Cambio nav y logo

--- src/app/componentes/logo/logo.tsx
+++ src/app/componentes/logo/logo.tsx
@@ -3,12 +3,10 @@ import './logo.css'
 export const Logo = () => {
 
     return ( 
-        <nav className="fondologo">
-            <div className="container logo">
-                <a className="navbar-brand" href="#">
-                    <img src="/imagenes/logomunicipio.jpeg" alt="logo de sierras bayas" width="370px" height="130px"/>
-                </a>
-            </div>
-        </nav>
+        <div className="container logo">
+            <a className="navbar-brand" href="#">
+                <img src="/imagenes/logomunicipio.jpeg" alt="logo de sierras bayas" width="370px" height="130px"/>
+            </a>
+        </div>
     );
 }   
