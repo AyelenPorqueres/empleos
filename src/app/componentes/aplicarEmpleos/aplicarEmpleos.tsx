e8f45dd43ad418f979e528d0b54dafdb55d41b44
Luna <lunacristofanelli@gmail.com>
2023-11-21 11:46:09 -0300

    agreguè formulario para aplicar empleos, modifique model/empleos con un array para guardar a los postulantes, los datos De estos se muestran por consola.

--- src/app/componentes/aplicarEmpleos/aplicarEmpleos.tsx
+++ src/app/componentes/aplicarEmpleos/aplicarEmpleos.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import './aplicarEmpleos.css';
-import Empleos from '@/app/model/Empleos';
+import Empleos, { agregarPostulante } from '@/app/model/Empleos';
 
 interface Empleo {
   id: number;
@@ -16,14 +16,12 @@ interface AplicarEmpleosProps {
 export function AplicarEmpleos(props: any) {
   const { empleo, handleClose } = props;
   const [misDatos, setMisDatos] = useState<Empleo[]>([{ id: empleo.id, mostrarInfo: true }]);
-
-  const mostrarModal = (id: number) => {
-    const newDatos: Empleo[] = misDatos.map((item) => ({
-      ...item,
-      mostrarInfo: item.id === id,
-    }));
-    setMisDatos(newDatos);
-  };
+  const [userInfo, setUserInfo] = useState({
+    nombre: '',
+    email: '',
+    numeroContacto: '',
+  });
+  const [isFormularioValido, setIsFormularioValido] = useState(false);
 
   const cerrarModal = (id: number) => {
     const newDatos: Empleo[] = misDatos.map((item) => ({
@@ -31,20 +29,63 @@ export function AplicarEmpleos(props: any) {
       mostrarInfo: item.id === id && false,
     }));
     setMisDatos(newDatos);
+
+    // Agregar el usuario a la lista de postulantes
+    agregarPostulante(userInfo);
+
+    // Mostrar por consola los datos del usuario
+    console.log('Datos del usuario:', userInfo);
+
+    // Marcar el formulario como válido
+    setIsFormularioValido(true);
+
+    // Cierra el modal
+    handleClose();
   };
 
   return (
     <>
-      <Modal className= 'modalEmpleos' show onHide={handleClose}>
+      <Modal className="modalEmpleos" show onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>¿Deseas aplicar a este empleo?</Modal.Title>
         </Modal.Header>
-        <Modal.Body> Empleo : {empleo} </Modal.Body>
+        <Modal.Body>
+          {misDatos.map((item) =>
+            item.mostrarInfo ? (
+              <form className="formEmpleos" key={item.id}>
+                <label htmlFor="nombre">Nombre Completo:</label>
+                <input
+                  type="text"
+                  id="nombre"
+                  name="nombre"
+                  value={userInfo.nombre}
+                  onChange={(e) => setUserInfo({ ...userInfo, nombre: e.target.value })}
+                />
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={userInfo.email}
+                  onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
+                />
+                <label htmlFor="numeroContacto">Número de Contacto:</label>
+                <input
+                  type="text"
+                  id="numeroContacto"
+                  name="numeroContacto"
+                  value={userInfo.numeroContacto}
+                  onChange={(e) => setUserInfo({ ...userInfo, numeroContacto: e.target.value })}
+                />
+              </form>
+            ) : null
+          )}
+        </Modal.Body>
         <Modal.Footer className="footerBotones">
-          <Button className= 'botonCancelar' variant="secondary" onClick={() => cerrarModal(empleo.id)}>
+          <Button className="botonCancelar" variant="secondary" onClick={() => cerrarModal(empleo.id)}>
             Cancelar
           </Button>
-          <Button className= 'botonAceptar' variant="primary" onClick={() => cerrarModal(empleo.id)}>
+          <Button className="botonAceptar" variant="primary" onClick={() => cerrarModal(empleo.id)}>
             Aplicar a Empleo
           </Button>
         </Modal.Footer>
@@ -52,4 +93,3 @@ export function AplicarEmpleos(props: any) {
     </>
   );
 }
-
--- src/app/componentes/cardEmpleos/cardEmpleos.tsx
+++ src/app/componentes/cardEmpleos/cardEmpleos.tsx
@@ -3,41 +3,41 @@ import React, { useState } from 'react';
 import './cardEmpleos.css';
 import Empleos from '@/app/model/Empleos';
 import Accordion from 'react-bootstrap/Accordion';
-import { AplicarEmpleos } from '../aplicarEmpleos/aplicarEmpleos'
+import { AplicarEmpleos } from '../aplicarEmpleos/aplicarEmpleos';
 
 export const CardEmpleos = (props: any) => {
-    const { datos }: { datos: Empleos[] } = props;
-    const [show, setShow] = useState(false);
+  const { datos }: { datos: Empleos[] } = props;
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    return (
-        <>
-            {datos.map((item: Empleos) => (
-                <div className="card d-flex flex-column align-items-center" key={item.id}>
-                    <img src={`imagenes/imagenesEmpleos/${item.puesto}.jpg`} className="card-img-top" alt={item.puesto} />
-                    <div className="card-body">
-                        <p className="card-text-title">{item.puesto}</p>
-                        <p className='card-text-description'>{item.descripcion}</p>
-                    </div>
-                    
-                    <Accordion flush>
-                        <Accordion.Item eventKey="0">
-                            <Accordion.Header className='button-vermas'>VER MÁS</Accordion.Header>
-                            <Accordion.Body className='accordion-body'>
-                                <p className='card-text-description'> RUBRO: {item.rubro}</p>
-                                <p className='card-text-description'> REQUISITOS: {item.requisitos}</p>
-                                <p className='card-text-description'> CARGA HORARIA: {item.cargaHoraria}hs</p>
-                                <button className='button-trabajo' onClick={handleShow}>
-                                    APLICAR AL TRABAJO
-                                </button>
-                                {show && <AplicarEmpleos empleo={item.rubro} handleClose={handleClose} />} 
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-                </div>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {datos.map((item: Empleos) => (
+        <div className="card d-flex flex-column align-items-center" key={item.id}>
+          <img src={`imagenes/imagenesEmpleos/${item.puesto}.jpg`} className="card-img-top" alt={item.puesto} />
+          <div className="card-body">
+            <p className="card-text-title">{item.puesto}</p>
+            <p className="card-text-description">{item.descripcion}</p>
+          </div>
+
+          <Accordion flush>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header className="button-vermas">VER MÁS</Accordion.Header>
+              <Accordion.Body className="accordion-body">
+                <p className="card-text-description"> RUBRO: {item.rubro}</p>
+                <p className="card-text-description"> REQUISITOS: {item.requisitos}</p>
+                <p className="card-text-description"> CARGA HORARIA: {item.cargaHoraria}hs</p>
+                <button className="button-trabajo" onClick={handleShow}>
+                  APLICAR AL EMPLEO
+                </button>
+                {show && <AplicarEmpleos empleo={item} handleClose={handleClose} />}
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
+      ))}
+    </>
+  );
 };
