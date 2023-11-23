01cfd00b38ba55f249b9b4794546a4fa8757ac82
AyelenPorqueres <ayelen.porqueres04@gmail.com>
2023-11-23 14:37:03 -0300

    se le agrego un link al boton ver mas

--- src/app/componentes/infociudad/info.tsx
+++ src/app/componentes/infociudad/info.tsx
@@ -1,3 +1,5 @@
+
+import Link from 'next/link';
 import './info.css';
 
 export const Info = () => {
@@ -10,6 +12,16 @@ export const Info = () => {
                 
                 <div className="divInfo">
                     <p>Sierras Bayas se ubica a 20 kilómetros de la ciudad de Olavarría. En dirección sudeste se direcciona la hermosa villa minera. Toma su nombre del color amarillento de la piedra que allí abunda, la dolomita. En Sierras Bayas se puede visitar antiguas fábricas de cal, hacer trekking por los senderos serranos, disfrutar de hermosas vistas desde lo alto de sus miradores y deslumbrarse con los lagos de cantera.</p>
+                    <button className='botonInfo'>
+                    <Link 
+                    className='links-vermas'  
+                    href={{pathname: '/cardturismo',}}
+                      >
+                      MÁS INFO
+                     </Link>
+                    
+                    
+                    </button>
                    
                 </div>
                 <div>
