52b73b54fb92c165d0bd3173f1a7bb61b5fc18d7
planeslorena <planeslorena@gmail.com>
2023-11-29 15:37:28 -0300

    correcion acceso a contacto y correccion toast

---
+++ src/app/(pages)/home/page.tsx
@@ -0,0 +1,36 @@
+import { Logo } from "../../componentes/logo/logo"
+import { Nav } from "../../componentes/nav/nav"
+import { Carrousel } from "../../componentes/carrousel/carrousel"
+import { Info } from "../../componentes/infociudad/info"
+import { Footer } from "../../componentes/footer/footer"
+import ServiciosComponent from "../../componentes/serviciosHome/servicios";
+import { Contacto } from "../../componentes/contacto/Contacto"
+import './page.css'
+import NumerosDeEmergencia from "../../componentes/numerosEmergencia/numerosEmergencia"
+
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <div className='fondologo'>
+          <Logo></Logo>
+        </div>
+
+        <Nav></Nav>
+
+      </header>
+
+      <main>
+        <Carrousel />
+        <Info></Info>
+        <ServiciosComponent />
+        <NumerosDeEmergencia></NumerosDeEmergencia>
+        <Contacto></Contacto>
+       
+      </main>
+
+      <Footer></Footer>
+    </>
+  )
+}
